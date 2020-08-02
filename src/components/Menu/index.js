692a8c8af1b3af5fecd6108f7e30356c47f23fe2
Prof Rafael <rafa.fernan10@gmail.com>
2020-08-02 16:57:17 -0300

    Páginas de Categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LogoImportado from '../../assets/img/Logo.png';
 import './Menu.css';
 //import ButtonLink from './components/ButtonLink';
@@ -8,10 +9,12 @@ import Button from '../Button';
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={LogoImportado} alt="Logo"/>
-            </a>
-            <Button className="ButtonLink" href="/">Novo vídeo</Button>
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo vídeo
+            </Button>
         </nav>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/home';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//  <React.StrictMode>
+ //   <App />
+ // </React.StrictMode>,
+ <BrowserRouter>
+  <Switch>
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    <Route path="/" component={Home} exact />
+    <Route component={()=>(<div>Página 404</div>)} />
+  </Switch>
+ </BrowserRouter>,
   document.getElementById('root')
 );
