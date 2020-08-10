114990d0dd09798a23c70e48c2e9de05c59ec8af
Prof Rafael <rafa.fernan10@gmail.com>
2020-08-10 13:08:01 -0300

    Cadastro de Vídeo com evento de teste no onSubmit

--- src/pages/cadastro/video/index.js
+++ src/pages/cadastro/video/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-// função de teste
+import useForm from '../../../hooks/useForm';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+
 function CadastroVideo() {
+  const { atualizarValores, values } = useForm({
+
+  });
   return (
     <PageDefault>
       <h1>Cadastro de Vídeo</h1>
 
+      <form onSubmit={(event) => {
+        event.preventDefault(); // evento ainda não está funcionando. Verificar motivo.
+        // eslint-disable-next-line no-alert
+        alert('Vídeo cadastrado com sucesso');
+      }}
+      >
+        <FormField
+          label="Título do vídeo"
+          name="titulo"
+          value={values.titulo}
+          onChange={atualizarValores}
+        />
+        <Button type="submit">
+          Cadastrar
+        </Button>
+      </form>
       <Link to="/cadastro/categoria">
         Cadastrar Categoria
       </Link>
